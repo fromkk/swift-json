6d4e9aa32b0a869f2fa5806edfca2892b1af8eab
sgr-ksmt <melodydance.k.s@gmail.com>
2016-06-03 11:47:30 +0900

    Add Carthage support

---
+++ swift-json/swift-json.h
@@ -0,0 +1,19 @@
+//
+//  swift-json.h
+//  swift-json
+//
+//  Created by Suguru Kishimoto on 2016/06/03.
+//  Copyright © 2016年 Dan Kogai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift-json.
+FOUNDATION_EXPORT double swift_jsonVersionNumber;
+
+//! Project version string for swift-json.
+FOUNDATION_EXPORT const unsigned char swift_jsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_json/PublicHeader.h>
+
+
